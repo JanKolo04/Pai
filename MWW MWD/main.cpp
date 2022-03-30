82a4c1f019636e0af038ff58252420b3e254532f
JanKolo04 <jankolodziej04@gmail.com>
2022-03-30 13:44:05 +0200

    algoritm for nww

--- MWW MWD/main.cpp
+++ MWW MWD/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int nwd();
+int nww();
+
 int main() {
+	nww();
+}
+
+int nwd() {
 	int a,b;
 
 	cout << "Podaj a: ";
@@ -20,4 +27,33 @@ int main() {
 		}
 	}
 	cout << a << endl;
+
+	return 0;
+}
+
+int nww() {
+	int a,b;
+
+	cout << "Podaj a: ";
+	cin >> a;
+
+	cout << "Podaj b: ";
+	cin >> b;
+
+	int iloczyn = a*b;
+
+	while(a!=b) {
+		if(a<b) {
+			b=b-a;
+		}
+		else {
+			a=a-b;
+		}
+	}
+
+	int nww = iloczyn/a;
+
+	cout << nww << endl;
+
+	return 0;
 }
--- liczby_pierwsze/main.cpp
+++ liczby_pierwsze/main.cpp
@@ -1,8 +1,15 @@
+#include <algorithm>
+#include <chrono>
 #include <iostream>
+#include<vector>
 using namespace std;
+using namespace std::chrono;
 
 
 int main() {
+
+	system("clear");
+
 	int ile_liczb;
 
 	cout << "Ile chcesz liczb: ";
@@ -12,6 +19,7 @@ int main() {
 	int liczba = 2;
 	bool test = true;
 
+	auto start = high_resolution_clock::now();
 	while(counter < ile_liczb) {
 		for(int i=2; i<liczba; i++) {
 			if(liczba%i == 0) {
@@ -27,6 +35,14 @@ int main() {
 		liczba++;
 	}
 	cout << endl;
+	auto stop = high_resolution_clock::now();
+	
+	auto duration = duration_cast<microseconds>(stop - start);
+	 
+	// To get the value of duration use the count()
+	// member function on the duration object
+	cout << "Do time: " << duration.count() << " microseconds" << endl;
+
 
 	return 0;
 }
