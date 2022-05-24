20bf9055774d2ef6c06d946b219b72a5a8059d30
JanKolo04 <jankolodziej04@gmail.com>
2022-05-24 14:55:08 +0200

    add time

--- wyszukiwanie_binarne/main.cpp
+++ wyszukiwanie_binarne/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 
+chrono::duration<double> elapsed_seconds;
+
 int main() {
+	chrono::system_clock::time_point start = chrono::system_clock::now();	// czas start
+
 	int l=0, p=15, sr=(l+p)/2;
 	int szukana;
 	int tab[15] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
@@ -12,6 +17,12 @@ int main() {
 	while(l<=p) {
 		if(tab[sr] == szukana) {
 			cout << "Liczba " << szukana << " znajduje się na pozycji: " << sr << endl;
+			chrono::system_clock::time_point end = chrono::system_clock::now(); // czas stop
+
+			elapsed_seconds += end - start;	// obliczenie czasu
+			cout << "\nCzas: ";
+			// wyswietlenie czasu w roznych jednostkach
+			cout << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_seconds).count()<< " milliseconds\n";
 			return 0;
 		}
 		else if(tab[sr] > szukana) {
@@ -23,4 +34,11 @@ int main() {
 		sr = (l+p)/2;
 	}
 	cout << "Nie znaleziono liczby" << endl;
+
+	chrono::system_clock::time_point end = chrono::system_clock::now(); // czas stop
+
+	elapsed_seconds += end - start;	// obliczenie czasu
+	cout << "\nCzas: ";
+	// wyswietlenie czasu w roznych jednostkach
+	cout << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_seconds).count()<< " milliseconds\n";
 }
